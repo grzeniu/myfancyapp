e13df8a5477ebf88bc3943d359cd413327ee1d05
Grzegorz <pg2464@o2.pl>
2022-03-31 12:13:20 +0200

    Add swagger and document product controller

--- src/main/java/com/example/myfanceapp/interfaces/rest/ProductController.java
+++ src/main/java/com/example/myfanceapp/interfaces/rest/ProductController.java
@@ -3,6 +3,10 @@ package com.example.myfanceapp.interfaces.rest;
 import com.example.myfanceapp.interfaces.rest.form.ProductForm;
 import com.example.myfanceapp.product.ProductService;
 import com.example.myfanceapp.product.model.Product;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,20 +29,46 @@ public class ProductController {
   private final ProductService service;
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductForm productForm) {
+  @Operation(summary = "Create new product", method = "POST")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "201", description = "Created"),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "500", description = "Server failure")
+      })
+  public ResponseEntity<Product> createProduct(
+      @Valid @RequestBody @Parameter(name = "productForm", description = "Product form")
+          ProductForm productForm) {
     Product newProduct = service.createProduct(productForm);
     return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
   }
 
   @GetMapping
+  @Operation(summary = "Get list of products", method = "GET")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "500", description = "Server failure")
+      })
   public ResponseEntity<List<Product>> getProducts() {
     List<Product> products = service.getProducts();
     return ResponseEntity.ok(products);
   }
 
   @PutMapping("/{product_id}")
+  @Operation(summary = "Update existing product", method = "GET")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "404", description = "Not found"),
+        @ApiResponse(responseCode = "500", description = "Server failure")
+      })
   public ResponseEntity<Product> updateProduct(
-      @PathVariable("product_id") Integer productId, @Valid @RequestBody ProductForm product) {
+      @PathVariable("product_id") @Parameter(description = "Product id", example = "123")
+          Integer productId,
+      @Valid @RequestBody @Parameter(name = "productForm", description = "Product form")
+          ProductForm product) {
     Product updatedProduct = service.updateProduct(productId, product);
     return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
   }
--- src/main/java/com/example/myfanceapp/interfaces/rest/form/ProductForm.java
+++ src/main/java/com/example/myfanceapp/interfaces/rest/form/ProductForm.java
@@ -1,6 +1,7 @@
 package com.example.myfanceapp.interfaces.rest.form;
 
 import com.example.myfanceapp.product.AsProduct;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductForm implements AsProduct {
 
+  @Schema(required = true, description = "Product name", example = "Product 1")
   private String name;
 
+  @Schema(required = true, description = "Product price", example = "12.34")
   private BigDecimal price;
 }
