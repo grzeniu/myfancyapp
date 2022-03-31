cedc7e0fea9808c51d0082d4c3b8b43eb17a1ea4
Grzegorz <pg2464@o2.pl>
2022-03-31 09:56:06 +0200

    Project initialization

---
+++ src/main/java/com/example/myfanceapp/MyfanceappApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfanceappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfanceappApplication.class, args);
+    }
+
+}
